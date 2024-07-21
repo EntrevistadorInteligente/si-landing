8f925837aedaa267f6220aece8aedcd3d6c5c7bc
Samuel Llibre-Pillco <dev@zyruks.com>
2024-07-21 14:34:17 -0400

    feat(components): ✨ add basic Button component
    Added components/Button directory with the following files:
    - Button.astro: Basic button component with variant, size, and other props.
    - Button.style.ts: Styling and variant definitions for the Button component using class-variance-authority.
    - Index.ts: Export for the Button component.
    
    chore(components): 🎨 re-organize some css classes

---
+++ src/components/Button/Button.style.ts
@@ -0,0 +1,83 @@
+import { cva, type VariantProps } from 'class-variance-authority';
+
+export const button = cva(
+  [
+    'relative',
+    'flex items-center justify-center',
+    'whitespace-nowrap text-center',
+    'duration-500 ease-elastic',
+    'border-2 border-transparent transition-shadow',
+    'shadow-button hover:shadow-hover active:shadow-sm',
+  ],
+  {
+    variants: {
+      variant: {
+        primary: 'bg-primary-500 text-white',
+        neutral: 'bg-neutral-900 text-gray-50',
+      },
+      isInvert: {
+        true: true,
+        false: false,
+      },
+      size: {
+        sm: 'h-9 px-4 py-1 text-xs font-semibold',
+        md: 'h-10 px-6 py-2.5 text-xs font-semibold',
+        lg: 'h-11 px-8 py-2 text-base font-semibold',
+        xl: 'h-12 px-10 py-3 text-base font-semibold',
+        '2xl': 'h-[3.75rem] px-12 py-2 text-lg font-semibold',
+      },
+      isFullRounded: {
+        true: 'rounded-full',
+        false: 'rounded-lg',
+      },
+      isFullWidth: {
+        true: 'w-full',
+        false: 'w-fit',
+      },
+      isOnlyIcon: {
+        true: '',
+        false: '',
+      },
+      isDisabled: {
+        true: 'disabled:cursor-not-allowed disabled:select-none disabled:border-neutral-200 disabled:bg-neutral-100 disabled:text-neutral-400 disabled:shadow-none disabled:hover:shadow-none',
+        false: '',
+      },
+    },
+    compoundVariants: [
+      {
+        isInvert: true,
+        variant: 'primary',
+        class: 'border-2 border-primary-600 bg-transparent text-primary-600',
+      },
+      {
+        isInvert: true,
+        variant: 'neutral',
+        class: 'border-2 border-neutral-900 bg-transparent text-black',
+      },
+      {
+        isOnlyIcon: true,
+        variant: 'primary',
+        class: 'text-black hover:text-gray-500',
+      },
+    ],
+    defaultVariants: {
+      size: 'md',
+      variant: 'primary',
+      isFullRounded: false,
+      isFullWidth: false,
+      isOnlyIcon: false,
+      isInvert: false,
+    },
+  },
+);
+
+export const icon = cva([''], {
+  variants: {
+    variant: {
+      primary: 'text-primary-700',
+      neutral: 'text-gray-800',
+    },
+  },
+});
+
+export type ButtonVariants = VariantProps<typeof button>;
