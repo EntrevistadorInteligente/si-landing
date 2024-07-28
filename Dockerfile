6a762911c3f539986871099480dd322aa59217b5
JamiltonQuintero <transportador1118@gmail.com>
2024-07-28 02:39:22 -0500

    Se agrega nginx y docker file. Se crean rutas bases para ruteos en cloudflare

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:lts AS build
+WORKDIR /app
+COPY package*.json ./
+RUN npm install
+COPY . .
+RUN npm run build
+
+FROM nginx:alpine AS runtime
+COPY ./nginx/nginx.conf /etc/nginx/nginx.conf
+COPY --from=build /app/dist /usr/share/nginx/html
+EXPOSE 4321
+
+CMD ["nginx", "-g", "daemon off;"]
--- src/common/constants/general.ts
+++ src/common/constants/general.ts
@@ -1,11 +1,11 @@
 // Assets paths
-export const ASSETS_PATH = '/assets';
+export const ASSETS_PATH = '/es/pagina-principal/assets';
 export const IMAGES_PATH = `${ASSETS_PATH}/images/`;
 
 //App paths
-export const HOME_PATH = '/';
-export const TERMS_AND_CONDITIONS_PATH = '/terms-and-conditions';
-export const PRIVACY_POLICY_PATH = '/privacy-policy';
+export const HOME_PATH = '/es/pagina-principal';
+export const TERMS_AND_CONDITIONS_PATH = '/es/pagina-principal/terms-and-conditions';
+export const PRIVACY_POLICY_PATH = '/es/pagina-principal/privacy-policy';
 
 // TODO: Update Social URL
 // Social paths
