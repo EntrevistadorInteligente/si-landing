d58b065b8590a9541b432d3cd8fe2b842fd1d637
Samuel Llibre-Pillco <dev@zyruks.com>
2024-08-02 14:48:39 -0400

    feat(constants): ✨ add new paths and API URL
    Added new paths: services, sample, who-we-are, and community.
    Introduced ANGULAR_APP_URL for API paths.

--- src/common/constants/general.ts
+++ src/common/constants/general.ts
@@ -4,9 +4,16 @@ export const IMAGES_PATH = `${ASSETS_PATH}/images/`;
 
 //App paths
 export const HOME_PATH = '/es/pagina-principal';
+export const SERVICES_PATH = '/es/pagina-principal/services';
+export const SAMPLE_PATH = '/es/pagina-principal/sample';
+export const WHO_WE_ARE_PATH = '/es/pagina-principal/who-we-are';
+export const COMMUNITY_PATH = '/es/pagina-principal/community';
+
 export const TERMS_AND_CONDITIONS_PATH = '/es/pagina-principal/terms-and-conditions';
 export const PRIVACY_POLICY_PATH = '/es/pagina-principal/privacy-policy';
 
+// API paths
+export const ANGULAR_APP_URL = import.meta.env.ANGULAR_APP_URL || 'http://localhost:4200/es/entrevistas';
 // TODO: Update Social URL
 // Social paths
 export const GITHUB_URL = '/';
