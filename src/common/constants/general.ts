7ac878cb98adc0c8c21d9d26ec0956b2b0cc81af
Francisco Mancuello Vazquez <franmavazq@gmail.com>
2024-08-03 03:51:31 -0400

    feat: :sparkles: update general constants links

--- src/common/constants/general.ts
+++ src/common/constants/general.ts
@@ -5,10 +5,10 @@ export const VIDEOS_PATH = `${ASSETS_PATH}/videos/`;
 
 //App paths
 export const HOME_PATH = '/es/pagina-principal';
-export const SERVICES_PATH = '/es/pagina-principal/services';
-export const SAMPLE_PATH = '/es/pagina-principal/sample';
-export const WHO_WE_ARE_PATH = '/es/pagina-principal/who-we-are';
-export const COMMUNITY_PATH = '/es/pagina-principal/community';
+export const SERVICES_PATH = '#services';
+export const SAMPLE_PATH = '#sample';
+export const WHO_WE_ARE_PATH = '#who-we-are';
+export const COMMUNITY_PATH = '#community';
 
 export const TERMS_AND_CONDITIONS_PATH = '/es/pagina-principal/terms-and-conditions';
 export const PRIVACY_POLICY_PATH = '/es/pagina-principal/privacy-policy';
@@ -17,7 +17,7 @@ export const PRIVACY_POLICY_PATH = '/es/pagina-principal/privacy-policy';
 export const ANGULAR_APP_URL = import.meta.env.ANGULAR_APP_URL || 'http://localhost:4200/es/entrevistas';
 // TODO: Update Social URL
 // Social paths
-export const GITHUB_URL = '/';
-export const TWITCH_URL = '/';
-export const LINKEDIN_URL = '/';
-export const DISCORD_URL = '/';
+export const GITHUB_URL = 'https://github.com/EntrevistadorInteligente';
+export const TWITCH_URL = 'https://www.twitch.tv/jamiltonqo';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/jamilton-quintero-osorio/';
+export const DISCORD_URL = 'https://discord.gg/2DUazgGCKr';
